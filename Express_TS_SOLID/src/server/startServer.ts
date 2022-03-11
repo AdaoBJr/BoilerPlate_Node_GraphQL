c336fff8bea858a4b23bcfc45cbfddb475585ea7
AdaoBJr <adaobjr@gmail.com>
2022-03-11 10:32:44 -0400

    feat: :boom: added new arch app startup

---
+++ Express_TS_SOLID/src/server/startServer.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import 'dotenv/config';
+import { router } from '../routers';
+
+const { PORT } = process.env;
+
+const startServer = () => {
+  const app = express();
+  app.use(express.json());
+  app.use(router);
+
+  app.listen(PORT, () => {
+    console.log(`🔥 Server Running on http://localhost:${PORT}`);
+  });
+};
+
+export { startServer };
